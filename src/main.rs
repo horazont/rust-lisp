ed25ed8af47a7ca5fa9a7a97cceb33e8e211ef3d
Jonas Schäfer <j.wielicki@sotecware.net>
2022-08-14 13:52:27 +0200

    Don't ask me why

---
+++ src/main.rs
@@ -0,0 +1,63 @@
+#![allow(unused_parens)]
+
+macro_rules! lisp_let {
+	(($n:ident $v:tt)) => {
+		let $n = lisp!{$v};
+	};
+	(($n:ident)) => {
+		let $n;
+	};
+}
+
+macro_rules! lisp_expr {
+	((let ($($lets:tt)*) $($block:tt)*)) => {
+		{
+			$(
+				lisp_let!{$lets};
+			)*
+			lisp!{$($block)*}
+		}
+	};
+	(($fn:ident $($x:tt)+)) => {
+		$fn($(lisp!($x),)+)
+	};
+	(($v:expr)) => {
+		$v
+	};
+}
+
+macro_rules! lisp {
+	((defun $n:ident ($($pn:ident: $pt:ty),*) -> $t:ty => $($block:tt)*)) => {
+		fn $n($($pn: $pt),*) -> $t {
+			lisp!{$($block)*}
+		}
+	};
+	($($item:tt)+) => {
+		{
+			$(
+				#[allow(unused_variables)]
+				let v = lisp_expr!{$item};
+			)+
+			v
+		}
+	};
+	() => {
+		()
+	}
+}
+
+lisp! {
+	(defun add (a: u32, b: u32) -> u32 => (
+		(a + b)
+		))
+}
+
+fn main() {
+	let foo = lisp! {
+		(let ((x (1)) (y (2)))
+			(add (x) (y))
+			(add (x) (y))
+			)
+	};
+	println!("{}", foo);
+}
